e5fd41dd7d4e0a58a909b77597a2a79b135235a1
dahee17 <133829893+dahee17@users.noreply.github.com>
2025-11-27 15:35:11 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import json
 
 app = Flask(__name__)
 SCHEDULE_FILE = "schedule.json"
-
 
 status = {
     "current_time": "00:00",
@@ -12,16 +11,22 @@ status = {
     "distance": 0
 }
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        hour = int(request.form["hour"])
-        minute = int(request.form["minute"])
-        with open(SCHEDULE_FILE, "w") as f:
-            json.dump({"hour": hour, "minute": minute}, f)
     with open(SCHEDULE_FILE, "r") as f:
         schedule = json.load(f)
     return render_template("index.html", schedule=schedule)
+
+@app.route("/save_time", methods=["POST"])
+def save_time():
+    hour = int(request.form["hour"])
+    minute = int(request.form["minute"])
+
+    with open(SCHEDULE_FILE, "w") as f:
+        json.dump({"hour": hour, "minute": minute}, f)
+
+    return redirect("/")
+    
 
 @app.route("/status")
 def get_status():
